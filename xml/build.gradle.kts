36a1658ae413d8440386a6bea1290ea5503b8a6c
EinNxk <einnxk@gmail.com>
2026-06-29 20:11:56 +0200

    chore: initial xml setup

---
+++ xml/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    kotlin("jvm") version "2.3.21"
+    id("com.gradleup.shadow") version "9.4.3"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":common"))
+    implementation(rootProject.libs.xml)
+    implementation(rootProject.libs.xml.kotlin)
+}
+
+kotlin {
+    jvmToolchain(25)
+}
+
+tasks.shadowJar {
+    archiveClassifier.set("")
+
+    relocate(
+        "com.fasterxml.jackson",
+        "com.github.einnxk.libs.xml"
+    )
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifact(tasks["shadowJar"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+
+            artifactId = "xml"
+        }
+    }
+}
+
+artifacts {
+    add("shadow", tasks.shadowJar)
+}
+
+tasks.jar {
+    enabled = false
+}
---
+++ xml/src/main/kotlin/com/github/einnxk/xml/XmlConfig.kt
@@ -0,0 +1,4 @@
+package com.github.einnxk.xml
+
+abstract class XmlConfig {
+}
