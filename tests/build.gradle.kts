bee1b3caeca59a54837cdf2fcff561be5dec8cd8
einnxk <einnxk@gmail.com>
2026-08-17 18:17:29 +0200

    ref: Refactored general gradle stuff

--- tests/build.gradle.kts
+++ tests/build.gradle.kts
@@ -1,11 +1,3 @@
-plugins {
-    kotlin("jvm") version "2.3.21"
-}
-
-kotlin {
-    jvmToolchain(25)
-}
-
 dependencies {
     testImplementation(project(":common"))
     testImplementation(project(":json", configuration = "shadow"))
--- yamler-paper/build.gradle.kts
+++ yamler-paper/build.gradle.kts
@@ -1,27 +1,15 @@
-plugins {
-    kotlin("jvm") version  "2.4.10"
-}
-
-repositories {
-    mavenCentral()
-    maven("https://repo.papermc.io/repository/maven-public/")
-}
-
 dependencies {
     implementation(project(":yamler-core", configuration = "shadow"))
 
-    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+    compileOnly(libs.paper)
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-}
-
-kotlin {
-    jvmToolchain(25)
 }
 
 publishing {
     publications {
         create<MavenPublication>("mavenJava") {
             from(components["java"])
+
             artifactId = "yamler-paper"
         }
     }
