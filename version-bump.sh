a32919b4d1e5fd4163b077e61f9ee483f80f4f4c
EinNxk <einnxk@gmail.com>
2026-09-06 01:52:56 +0200

    Automated Releases (#48)
    * chore: Introduced util files
    
    * chore: Introduced auto release script

---
+++ version-bump.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+VERSION_FILE="version-info.json"
+CURRENT_VERSION=$(jq -r '.version' "$VERSION_FILE")
+IFS='.' read -r MAJOR MINOR PATCH <<< "$CURRENT_VERSION"
+NEW_VERSION="${MAJOR}.$((MINOR + 1)).0"
+TMP_FILE=$(mktemp)
+jq --arg v "$NEW_VERSION" '.version = $v' "$VERSION_FILE" > "$TMP_FILE"
+mv "$TMP_FILE" "$VERSION_FILE"
+echo "$NEW_VERSION"
