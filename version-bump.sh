a6eba8a081429be680c445c03830621a8e65c996
EinNxk <einnxk@gmail.com>
2026-09-06 02:17:29 +0200

    Fixed auto releases (#50)
    * patch: Fixed auto-releases
    
    * patch: Fixed pr generation
    
    * chore: small adjustions

--- version-bump.sh
+++ version-bump.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 VERSION_FILE="version-info.json"
 CURRENT_VERSION=$(jq -r '.version' "$VERSION_FILE")
 IFS='.' read -r MAJOR MINOR PATCH <<< "$CURRENT_VERSION"
-NEW_VERSION="${MAJOR}.$((MINOR + 1)).0"
+NEW_VERSION="${MAJOR}.${MINOR}.$((PATCH + 1))"
 TMP_FILE=$(mktemp)
 jq --arg v "$NEW_VERSION" '.version = $v' "$VERSION_FILE" > "$TMP_FILE"
 mv "$TMP_FILE" "$VERSION_FILE"
